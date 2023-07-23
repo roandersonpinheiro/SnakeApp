68c296d8284d8a9239201dd772d43437a66f5a1a
Roanderson Pinheiro <roandersonfelipepinheiro@gmail.com>
2023-07-23 12:07:17 -0300

    refactor: Refactor Theme.kt

--- app/src/main/java/com/example/snakeapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/snakeapp/ui/theme/Type.kt
@@ -1,34 +1,88 @@
 package com.example.snakeapp.ui.theme
-
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.snakeapp.R
 
-// Set of Material typography styles to start with
+private val appFontFamily = FontFamily(fonts = listOf(Font(resId = R.font.snake)))
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = appFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        fontSize = 57.sp,
+    ),
+//    displayMedium = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 45.sp,
+//    ),
+//    displaySmall = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 36.sp,
+//    ),
+//    headlineLarge = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 32.sp,
+//    ),
+//    headlineMedium = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 28.sp,
+//    ),
+//    headlineSmall = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 24.sp,
+//    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = appFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+//    titleMedium = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Medium,
+//        fontSize = 16.sp,
+//    ),
+//    titleSmall = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Medium,
+//        fontSize = 14.sp,
+//    ),
+//    bodyLarge = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp,
+//    ),
+//    bodyMedium = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 14.sp,
+//    ),
+//    bodySmall = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 12.sp,
+//    ),
+//    labelLarge = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Medium,
+//        fontSize = 14.sp,
+//    ),
+//    labelMedium = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Medium,
+//        fontSize = 12.sp,
+//    ),
+//    labelSmall = TextStyle(
+//        fontFamily = appFontFamily,
+//        fontWeight = FontWeight.Medium,
+//        fontSize = 11.sp,
+//    ),
 )
